f1292106857e471fd6371d60b88ae2b0c76c7b04
shsa-odoo <shsa@odoo.com>
2022-04-13 18:38:42 +0530

    Added a new project Co-operative volunteers

---
+++ co-operative-volunteers/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details. a dictionary, each key in this dctionary is module
+{
+    'name' : 'Co-operative Volunteers',
+    'version' : '1.0',
+    'summary': 'A consolidated tool for library management',
+    'sequence': 13,
+    'description': """ A co operative volunteers management system """
+    'category': 'Hr/Planner/Contacts',
+    'depends' : ['base_setup'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
