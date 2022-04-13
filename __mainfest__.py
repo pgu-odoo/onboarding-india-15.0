092a0a3614f9173c535251fef7dabf29c65e9bcd
shsa-odoo <shsa@odoo.com>
2022-04-13 15:26:04 +0530

    changes on manifest

--- __mainfest__.py
+++ __mainfest__.py
@@ -3,16 +3,13 @@
 {
     'name' : 'Open Academy',
     'version' : '1.0',
-    'summary': 'Invoices & Payments',
+    'summary': 'Open Academy',
     'sequence': 10,
-    'description': """
-    Open Academy
-====================
-The specific and easy-to-use Institutional system in Odoo allows you to keep track of your day to day management of Institutional activites, even when you are not an Expert. It provides an easy way to follow up on your Staffs and Students.
-
-You could use this simplified Management in case you work with an (external) account to keep your ledger, and you still want to keep track of activites. This module also offers you an easy method of registering, without having to encode complete abstracts of information.
-    """,
-    'category': 'Planner/Helpdesk/Payroll',
+    'description': """  Academy module to manage 
+    Training
+    Sessions
+    Course Attendees """
+    'category': 'Training',
     'depends' : ['base_setup'],
     'installable': True,
     'application': True,
