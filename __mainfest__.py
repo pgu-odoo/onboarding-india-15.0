182727b9540feca47a79886c6c9f73ef515edc75
shsa-odoo <shsa@odoo.com>
2022-04-13 11:41:27 +0530

    created init and manifest files

---
+++ __mainfest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Invoicing',
+    'version' : '1.2',
+    'summary': 'Invoices & Payments',
+    'sequence': 10,
+    'description': """
+    Open Academy
+====================
+The specific and easy-to-use Institutional system in Odoo allows you to keep track of your day to day management of Institutional activites, even when you are not an Expert. It provides an easy way to follow up on your Staffs and Students.
+
+You could use this simplified Management in case you work with an (external) account to keep your ledger, and you still want to keep track of activites. This module also offers you an easy method of registering, without having to encode complete abstracts of information.
+    """,
+    'category': 'Planner/Helpdesk/Payroll',
+    'depends' : ['base_setup'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
