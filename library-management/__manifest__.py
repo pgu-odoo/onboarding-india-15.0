d013c72554ab125ee3c05bfb0216a25c73ba8c42
shsa-odoo <shsa@odoo.com>
2022-04-13 18:33:09 +0530

    added new project for library management

---
+++ library-management/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details. a dictionary, each key in this dctionary is module
+{
+    'name' : 'Library Mangement Module',
+    'version' : '1.0',
+    'summary': 'A consolidated tool for library management',
+    'sequence': 12,
+    'description': """  A complete module for library managemnet
+    maintains Books and Customers
+    Cutomer checkout book
+    and organizing """
+    'category': 'Account/Contacts/Subscriptions & Timesheet',
+    'depends' : ['base_setup'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
