9bc68aecdbb8739f18dd193ee33cd0ac9d4084c9
shsa-odoo <shsa@odoo.com>
2022-04-15 14:18:52 +0530

    manifest change

--- open-academy/__mainfest__.py
+++ open-academy/__mainfest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details. a dictionary, each key in this dctionary is module
+
 {
     'name' : 'Open Academy',
     'version' : '1.0',
