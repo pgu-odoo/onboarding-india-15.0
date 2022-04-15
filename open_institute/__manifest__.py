c021b6befe14fb0ccd2a0bf77cdd75345a33d180
shsa-odoo <shsa@odoo.com>
2022-04-15 14:49:28 +0530

    added open academy

---
+++ open_institute/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details. a dictionary, each key in this dctionary is module
+{
+    'name' : 'Odoo Open Institue',
+    'version' : '1.0',
+    'summary': 'Open institute Mission',
+    'sequence': 999,
+    'description': """  A exciting Space Mission by Odoo 
+    maintains spacecrafts and crews """,
+    'category': 'Planning/Sales/Contacts',
+    'depends' : ['base_setup'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ open_institute/models/course.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class course(models.Model):
+	_name = 'open.course'
+	_description = 'open institute module'
+	
