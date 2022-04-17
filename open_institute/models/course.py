7636bc18c39aa80a8ecd83c46ee0b40065ecd97c
shsa-odoo <shsa@odoo.com>
2022-04-17 17:10:41 +0530

    demo data added

--- open_institute/models/course.py
+++ open_institute/models/course.py
@@ -5,3 +5,13 @@ class course(models.Model):
 	_name = 'open.course'
 	_description = 'open institute module'
 	
+	name = fields.Char(string="Title", required=True)
+	description= fields.Text(string='Description')
+
+	level = fields.Selection(string='Level',
+							  selection= [('beginner', 'Beginner'),
+							  			  ('intermediate', 'Intermediate'),
+							  			   ('advanced', 'Advanced')],
+							  copy = False)
+
+	active = fields.Boolean(string= 'Active', default=True)
