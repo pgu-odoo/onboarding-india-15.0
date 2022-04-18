15392ba6abf9d39213077ff83a3e88cd8ab5490e
mega-odoo <mega@odoo.com>
2022-04-18 18:26:06 +0530

    new module construction

--- construction/__manifest__.py
+++ construction/__manifest__.py
@@ -2,12 +2,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-	'name' : 'Estate',
+	'name' : 'construction',
 
-	'summary' : """Real Estates based module""",
+	'summary' : """construction based module etc..""",
 
 	'description' : """
-		Application for Real Estate based work:
+		Application for construction based work:
 		-Public Sector
 		-Private Housing
 		-Skyhigh Projects
@@ -15,39 +15,14 @@
 
 	'author' : 'Odoo',
 
-	'website' : 'www.odoo.com',
+	'website' : 'https://www.odoo.com',
 
 	'category' : 'Sales',
 	'version' : '1.0',
-	'license' : 'LGPL-3',
 
-	'depends' : [
-					'base',
-					'website',
-					'mail',
-					# 'website_sale'
-				],
-
-	'data' : [
-		'security/estate_security.xml',
-		'security/ir.model.access.csv',
-		'views/real_estate_property_view.xml',
-		'views/real_estate_property_menu.xml',
-		'views/real_estate_property_type.xml',
-		'views/real_estate_property_tag.xml',
-		'views/real_estate_property_offer.xml',
-		'wizards/property_wizard_view.xml',
-		'report/estate_property_templates.xml',
-		'report/estate_property_reports.xml',
-		'views/real_estate_web.xml',
-		# 'data/data.xml', # not needed right now
-	],
+	'depends' : [],
+    'data' : [],
 
 	################### commented because it was causing problems in further tasks
-	# 'demo' : [
-	# 	'demo/estate_demo.xml',
-
-	# ],
-	'auto_install' : True,
-	'application': True,
+	'demo' : [],
 }
