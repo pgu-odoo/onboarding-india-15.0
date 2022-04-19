bcc8212d4b6e08b624bf3796146cd3617604af64
shsa-odoo <shsa@odoo.com>
2022-04-19 23:03:44 +0530

    [modification]

--- open_institute/models/course.py
+++ open_institute/models/course.py
@@ -17,4 +17,14 @@ class Course(models.Model):
 							  			   ('advanced', 'Advanced')],
 							  copy = False)
 
-	active = fields.Boolean(string= 'Active', default=True)
+	active = fields.Boolean(string='Active', default=True)
+
+	base_price = fields.Float(string='Base Price', default=0.00)
+
+	additional_price = fields.Float(string='Additional Price', default=0.00)
+
+	total_price = fields.Float(string='Total Price', readonly=True)
+
+	@api.onchange('base_price','additional_price')
+	def _onchange_total_price(self):
+		 self.total_price = self.base_price + self.additional_price
