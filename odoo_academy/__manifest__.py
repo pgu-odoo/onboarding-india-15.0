25eafaab31664ebbb2abc05417abeab9c5fe6c3d
kena-odoo <kena@odoo.com>
2022-04-25 15:22:04 +0530

    [add] fapp

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,22 @@
+# -- coding: utf-8 -
+{
+    'name':' Odoo Acedamy',
+    'summary':""" Acedamy app to manage training""",
+    'description':""" Acedamy app to manage training:
+                        - courses
+                        - sessions
+                        - attendees
+                """ ,
+    'author':'odoo',
+    'website':'https://www.odoo.com/',
+    'category':'Training',
+    'version':'0.1',
+    'depends':['base_setup'],
+    # 'data':[
+
+    # ],
+    # 'demo':[
+
+    # ],
+    'installable':'True'
+}
