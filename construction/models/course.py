17c6621a04f93b9c4f8a190c386f0779e23b2177
mega-odoo <mega@odoo.com>
2022-04-26 19:02:40 +0530

    Apr 26 2022

--- construction/models/course.py
+++ construction/models/course.py
@@ -13,17 +13,16 @@ class Course(models.Model):
 
 	name =fields.Char(string='Title',required=True) #required True bacause we always have a title
 	description=fields.Text(string='Description')
-
 	level=fields.Selection(string='Level',selection=[('beginner','Beginner'),('intermediate','Intermediate'),('advanced','Advanced')],copy=False,default='beginner')
-
 	active =fields.Boolean(string='Active',default=True)
 
 	base_price=fields.Float(string="Base Price",default=0.00)
 	additional_fee=fields.Float(string="Additional_fee",default=10.00)
 	total_price=fields.Float(string="Total_Price",compute='_compute_total_price')
 
-	subject = fields.Many2one(comodel_name='subject.details',string="Subject")
-	add_subject = fields.One2many('subject.details','details_id',string="Subject Options")
+	subject_ids = fields.Many2one(comodel_name='subject.details',string="Subject")                  # ids_name = fields.Many2one('relation_model_name',string='',ondelete='cascade')
+	add_subject_ids = fields.One2many('subject.details','details_id',string="Subject Options")      # ids_name = fields.One2many('relation_model_name','inverse_name',string='')
+	# subjects_ids=fields.Many2many(comodel_name='subject.details',string="Subjects")
 
 
 	# @api.onchange('base_price','additional_fee')  # invoke when we  change in  "additional_fee"  and "base_price" and we can changes in total_price
@@ -37,63 +36,58 @@ class Course(models.Model):
 	@api.depends('base_price','additional_fee') # invoke when we  change in  "additional_fee"  and "base_price" and we can't changes in total_price
 	def _compute_total_price(self):
 		print("base_price------------------>",self.base_price)
-		if self.base_price<0.00:
+		if self.base_price < 0.00:
 			raise UserError('base price cannot be set as nagative')
 
-		self.total_price=self.base_price+self.additional_fee
+		self.total_price = self.base_price +  self.additional_fee
 
 
 
-	@api.onchange('additional_fee') # invoke when we  change in  "additional_fee" 
-	def _check_additional_fee(self):
-		for record in self:
-			print('self ------------------>',record)
-			if record.additional_fee<10.00:
-				raise ValidationError('additional_fee cannot be less than 10: %s'%record.additional_fee)
+	# @api.onchange('additional_fee') # invoke when we  change in  "additional_fee" 
+	# def _check_additional_fee(self):
+	# 	for record in self:
+	# 		print('self ------------------>',record)
+	# 		if record.additional_fee<10.00:
+	# 			raise ValidationError('additional_fee cannot be less than 10: %s'%record.additional_fee)
 
-	@api.model
-	def create(self,vals): # invoke when we click on create+save button 
-		print("create  method------------------>",vals)
-		if vals.get('name') == 'meet':
-			vals['name'] = 'Riddhi'
-		res = super(Course,self).create(vals)
-		return res
-		# super(classname,self).craete(other parameer which given in function)
+	# @api.model
+	# def create(self,vals): # invoke when we click on create+save button 
+	# 	print("create  method------------------>",vals)
+	# 	if vals.get('name') == 'meet':
+	# 		vals['name'] = 'Riddhi'
+	# 	res = super(Course,self).create(vals)
+	# 	return res
+	# 	# super(classname,self).craete(other parameer which given in function)
 
 	
-	def write(self,vals):  #no decorators needed for  write method  invoke when we click on Edit+save button
-		print("write method------------------>",vals)
-		if vals.get('name') == 'meet':
-			vals['name'] = 'Meet'
-		res = super(Course,self).write(vals)
-		return res
+	# def write(self,vals):  #no decorators needed for  write method  invoke when we click on Edit+save button
+	# 	print("write method------------------>",vals)
+	# 	if vals.get('name') == 'meet':
+	# 		vals['name'] = 'Meet'
+	# 	res = super(Course,self).write(vals)
+	# 	return res
 
-	@api.constrains('description')
-	def check_description(self):
-		for rec in self:
-			note=self.env['academy.course'].search([('description', '=', rec.description)])
-			if note:
-				raise ValidationError("description is  %s already exists",rec.description)
-
-
+	# @api.constrains('description')
+	# def check_description(self):
+	# 	for rec in self:
+	# 		note=self.env['academy.course'].search([('description', '=', rec.description)])
+	# 		if note:
+	# 			raise ValidationError("description is  %s already exists",rec.description)
 
 
-	@api.onchange('base_price','additional_fee')  # invoke when we  change in  "additional_fee"  and "base_price" and we can changes in total_price
-	def _onchange_total_price(self):
-		browse_method=self.env['academy.course'].browse([15,14,1])
+
+
+	# @api.onchange('base_price','additional_fee')  # invoke when we  change in  "additional_fee"  and "base_price" and we can changes in total_price
+	# def _onchange_total_price(self):
+	# 	browse_method=self.env['academy.course'].browse([15,14,1])
 		
-		print('-------------------------------------->>>',browse_method) #browse_method=academy.course(15, 14, 1)
+	# 	print('-------------------------------------->>>',browse_method) #browse_method=academy.course(15, 14, 1)
 
-		for i in browse_method: # i=academy.course(1,) ,i=academy.course(15,),i=academy.course(14,) 
-			print(i.additional_fee)  
+	# 	for i in browse_method: # i=academy.course(1,) ,i=academy.course(15,),i=academy.course(14,) 
+	# 		print(i.additional_fee)  
 
-		# search_method=self.env['academy.course'].search([('partner_id', '=', seller.id)])
+	# 	# search_method=self.env['academy.course'].search([('partner_id', '=', seller.id)])
         
-
-
-
-
-	
 
 
 class SubjectDetails(models.Model):
@@ -105,4 +99,4 @@ class SubjectDetails(models.Model):
 	subject = fields.Char('Subject')
 	book = fields.Char('Book Name')
 	author = fields.Char("Author")
-	details_id = fields.Many2one('academy.course',string="details")
+	details_id = fields.Many2one('academy.course',string="details")	# id = fields.Many2one('relational.object.name',string='')
