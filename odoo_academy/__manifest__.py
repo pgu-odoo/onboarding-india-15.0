73a6795074acb42de945840f70b1d0c2ecb1e46a
kena-odoo <kena@odoo.com>
2022-04-28 16:32:20 +0530

    foamv

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -15,7 +15,9 @@
     'installable':'True',
     'data':[
         'security/academy_security.xml',
-        'security/ir.model.access.csv',],
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',],
     'demo':['demo/academy_demo.xml',
     ]
 
