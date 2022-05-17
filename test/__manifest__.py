fa6850369c6bb11c16d9d957ae715c18226ec5bd
aath-odoo <aath@odoo.com>
2022-05-17 16:53:06 +0530

    commit for day one technical training

---
+++ test/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Test',
+    'version': '15.0',
+    'summary': 'day one technical training',
+    'description': 'description for the module',
+    'author': 'Test',
+    'website': 'odoo.com',
+    'depends': ['base'],
+    'data': [
+
+    ],
+
+    'installable': True,
+    'auto_install': False
+}
