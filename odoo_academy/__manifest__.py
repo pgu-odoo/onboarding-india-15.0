3ddde60235b2c15c9aaebe105680027a50bbdf76
Saikiran1410 <ellursaikiran@gmail.com>
2022-05-26 18:29:54 +0530

    Day 1 commit

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name":"odoo_academy",
+    "version":"15.0.0.1",
+    "summary":"odoo training",
+    "description":"training module",
+    "category":"learning",
+    "depends":["base"],
+    "data":[
+
+    ],
+    "installable":True,
+    "application":True,
+}
