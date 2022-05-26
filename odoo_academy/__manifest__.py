69bbfb023993b2542fa63cd9e5740bb13d41261a
poka-odoo <poka@odoo.com>
2022-05-26 17:07:17 +0530

    initial commit

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+	'name': 'Odoo Academy',
+
+	'summary': """ Academy app for learning """,
+
+	'description': """ 
+		Academy module for learning 
+	""",
+
+	'author': 'Odoo',
+
+	'website': 'https:/www.odoo.com',
+
+	'category': 'Learning',
+
+	'version': '15.0.1',
+
+	'depends': ['base'],
+
+	'data':[
+	
+	],
+
+	'demo': [
+
+	],
+}
