d4dabe5872f7629a4d2ee17de32d902c4cccc52d
Saikiran1410 <ellursaikiran@gmail.com>
2022-06-01 18:13:05 +0530

    Day 4 commit

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,4 +1,5 @@
 
+from pickle import TRUE
 import string
 from odoo import models,fields,api
 
@@ -9,13 +10,14 @@ class Course(models.Model):
     _description = "Course Info"
     
     
-    name = fields.Char(string="Title")
-    description = fields.Text(string="Description")
+    name = fields.Char(string="Title",placeholder="Enter the course name ex. Maths")
+    description = fields.Text(string="Description",help="Enter Description",readonly=False)
 
     level = fields.Selection(string="Level",
-    selection=[("Beginner","Beginner"),("Intermediate","Intermediate"),("Advanced","Advanced")],copy=False)
+                selection = [("beginner","Beginner"),("intermediate","Intermediate"),("advanced","Advanced")],default='advanced')
 
     active=fields.Boolean(string="Active", default=True)
+
     price = fields.Float(string="price")
     end_course_date = fields.Datetime(string='End Date')
     start_course_date = fields.Date(string='Start Date')
