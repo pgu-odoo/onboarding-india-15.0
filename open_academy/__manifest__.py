6b236e62f5a2bc9f1a9be9fb8ce80802021d44c7
zekh-odoo <zekh@odoo.com>
2022-06-13 12:35:39 +0530

    [ADD] added new module

---
+++ open_academy/__manifest__.py
@@ -0,0 +1,17 @@
+
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Open Academy',
+    'version' : '1.0',
+    'summary': 'Module for testing',
+    'sequence': 1,
+    'description': """testing""",
+    'website': 'https://www.odoo.com/',
+    'depends' : ['base_setup'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+   }
