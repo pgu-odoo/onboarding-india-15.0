2762348507037e0a0facc96c680f1e036a7364cd
yaut-odoo <yaut@odoo.com>
2022-06-14 14:49:31 +0530

    [ADD] added lbrary module

---
+++ odoo_library/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Library',
+    'version': '1.0',
+    'description': """Library Management""",
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'license': 'LGPL-3',
+}
