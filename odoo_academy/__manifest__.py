a72015aff99702b0bff772ad8377abe886ca6a8f
yaut-odoo <yaut@odoo.com>
2022-06-14 11:22:46 +0530

    [ADD] added new module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'version': '1.0',
+    'description': """test odoo""",
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'license': 'LGPL-3',
+}
