b2bfbf099eb9763faa2415efcdbb3c1b021e8103
zekh-odoo <zekh@odoo.com>
2022-06-14 14:32:57 +0530

    [ADD] Basic Module

---
+++ open_space/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name' : 'Open Space',
+    'version' : '1.0',
+    'summary': 'Module for testing',
+    'sequence': 1,
+    'description': """testing""",
+    'website': 'https://www.odoo.com/',
+    'depends' : ['base_setup'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
