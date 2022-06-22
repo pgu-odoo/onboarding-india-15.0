3a8dd9b35b74f06b49e38ca79126640662121429
yaut-odoo <yaut@odoo.com>
2022-06-22 17:40:18 +0530

    commit

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -14,6 +14,7 @@
         'views/sale_views_inherit.xml',
         'views/product_views_inherit.xml',
         'wizards/sale_wizard_view.xml',
+        'report/session_report_templates.xml',
     ],
 
     'demo':[
