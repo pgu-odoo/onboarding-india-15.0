8a01e8a3aedcfaf64ebf64dbeefd5684d79c3b5b
shah-odoo <shah@odoo.com>
2022-06-24 12:06:36 +0530

    [ADD] added module odoo academy

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name' : 'Odoo academy',
+    'version' : '1.0',
+    'summary': 'App for odoo academy',
+    'sequence': 1,
+    'description': """Test """,
+     'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    }
