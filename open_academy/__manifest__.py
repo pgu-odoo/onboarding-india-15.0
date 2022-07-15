4820d8a55ae9379d2b258cc52be3ec07794b4072
miso-odoo <miso@odoo.com>
2022-07-15 10:33:37 +0530

    test

---
+++ open_academy/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Odoo Academy',
+    'version': '1.0',
+    'description': """
+        A module for odoo academy
+    """,
+    'depends': ['base'],
+    'data': [],
+    'license': 'LGPL-3',
+}
