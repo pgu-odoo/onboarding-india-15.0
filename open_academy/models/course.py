55f0809e04db5e8995092f709a02b93398ff0e18
miso-odoo <miso@odoo.com>
2022-07-20 10:33:54 +0530

    test

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -5,3 +5,23 @@ class Course(models.Model):
 	_name = 'academy.course'
 	_discription = 'Course Info'
 	
+
+	description = fields.Text(string='Description')
+
+	level = fields.Selection(string='Level', 
+							 selection=[('beginner', 'Beginner'),
+										('intermediate', 'Intermediate'),
+										('advanced', 'Advanced')],
+							 copy=False)
+	
+	active = fields.Boolean(string='Active', default=True)
+	designation = fields.Char(string='Title', required=True)
+	e_salary = fields.Char(string='salary', required=True)
+	first_name = fields.Char(string='name', required=True)
+	last_name = fields.Char(string='name', required=True)
+
+
+
+
+
+
