e9c61014d7ac7ec19bc029be47c996fa58f385da
shah-odoo <shah@odoo.com>
2022-07-26 16:55:33 +0530

    committed

--- odoo_academy/models/product_template.py
+++ odoo_academy/models/product_template.py
@@ -6,5 +6,5 @@ class ProductTemplate(models.Model):
 	_inherit='product.template'
 
 	is_session_product=fields.Boolean(string="Use as Session Product",
-		                               help="Check this box is use as a product for session fee"
+		                               help="Check this box is use as a product for session fee" 
 		                               ,default=False)
