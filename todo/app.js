d53bb0ca552f9354458f216a2bccdbee840ab34b
can-odoo <can@odoo.com>
2022-08-24 15:35:54 +0530

    ToDo owl: Add Ne task, check task by state

--- todo/app.js
+++ todo/app.js
@@ -1,7 +1,8 @@
-const { Component, useState, xml, mount, useRef, onMounted, reactive, useEnv } = owl;
+const { Component, useState, xml, mount, useRef, onMounted, reactive, useEnv } = owl;  // Q1
 
 (function () {
     console.log("hello owl", owl.__info__.version);
+    console.log(owl);
 })();
 
 class Counter extends Component {
@@ -9,126 +10,162 @@ class Counter extends Component {
   state = useState({ value: 0, de_value: 100 });
   
   increment() {
-	this.state.value++;
+    this.state.value++;
   }
   decrement() {
-	this.state.de_value--;
+    this.state.de_value--;
   }
-}
-
-// Local store
-function useStore () {
-	const env = useEnv();
-	return useState(env.store);
 }
 
 
 // template can be used like this as well
 Counter.template = xml /*xml*/`
-	<button t-on-click="increment">
-		Click Me! [<t t-esc="state.value"/>]
-	</button><br/>
-	<button t-on-click="decrement">
-		Click Me decrement! [<t t-esc="state.de_value"/>]
-	</button>`;
+    <button t-on-click="increment">
+        Click Me! [<t t-esc="state.value"/>]
+    </button><br/>
+    <button t-on-click="decrement">
+        Click Me decrement! [<t t-esc="state.de_value"/>]
+    </button>`;
 
 mount(Counter, document.body);
 
+// Local store
+function useStore () {
+    const env = useEnv();
+    console.log("useState(env.store): ", useState(env.store));
+    return useState(env.store);
+}
 
+
+class TaskList {
+
+    constructor(task) {
+        this.tasks = this.tasks || [];
+        const taskId = this.tasks.map((t) => t.id);
+        this.nextId = taskId.length ? Math.max(...taskId) + 1 : 1;
+    }
+
+    // nextId = 1;
+    // tasks = [];
+    
+    // add new task
+    addTask(text) {
+        text = text.trim();
+        if (text) {
+            const task = {
+                id: this.nextId++,
+                text: text,
+                isCompleted: false,
+            };
+            this.tasks.push(task)
+        }
+    }
+    
+    // toggle task, when isCompleted is true
+    toggleTask(task) {
+        task.isCompleted = !task.isCompleted;
+    }
+    
+    // delete task
+    deleteTask(task) {
+        const index = this.tasks.findIndex( (t) => t.id === task.id );
+        this.tasks.slice(index, 1);
+    }
+}
+
+// store task
+function createTaskStore() {
+    const saveTasks = () => localStorage.setItem("todoapp", JSON.stringify(taskStore.tasks));
+    const initialTasks = JSON.parse(localStorage.getItem("todoapp") || "[]");
+    const taskStore = reactive(new TaskList(initialTasks), saveTasks);
+    saveTasks();
+    console.log(taskStore);
+    return taskStore;
+}
 
 // Task Components (sub component)
 class Task extends Component {
-	static template = xml /* xml*/`
-	<div class="task" t-att-class="props.task.isCompleted ? 'done': ''">
-		<input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="toggleTask" />	
-		<span><t t-esc="props.task.text" /></span>
-		<span class="delete" t-on-click="deleteTask">🗑</span>
-	</div>
-	`;
-	static props = ["task", "onDelete"];
+    static template = xml /* xml*/`
+    <div class="task" t-att-class="props.task.isCompleted ? 'done': ''">
+        <input type="checkbox" t-att-checked="props.task.isCompleted"
+               t-att-id="props.task.id"
+               t-on-click="() => store.toggleTask(props.task)" />
+        <label t-att-for="props.task.id"><t t-esc="props.task.text" /></label>
+        <span class="delete" t-on-click="() => store.deleteTask(props.task)">🗑</span>
+    </div>
+    `;
+    static props = ["task"];  // Q2
 
-	toggleTask () {
-		this.props.task.isCompleted = !this.props.task.isCompleted;
-	}
-
-	deleteTask () {
-		this.props.onDelete(this.props.task);
-	}
-}
-
-class TaskList {
-	nextId = 1;
-	tasks = []
-
-	// add new task
-	addTask(text) {
-		text = text.trim();
-		if (text) {
-			const task = {
-				id: this.nextId++,
-				text: text,
-				isCompleted: false,
-			};
-			this.tasks.push(task)
-		}
-	}
-
-	// toggle task, when isCompleted is true
-	toggleTask(task) {
-		task.isCompleted = !task.isCompleted;
-	}
-
-	// delete task
-	deleteTask(task) {
-		const index = this.tasks.findIndex( (t) => t.id === task.id );
-		this.tasks.slice(index, 1);
-	}
+    setup() {
+        this.store = useStore();
+    }
 }
 
 
 // ====================================
 
 class Root extends Component {
-	static template = xml /* xml */`
-	<div class="todo-app">
-		<input placeholder="Enter new Task" t-on-keyup="addTask" t-ref="add-input"/>
-		<div class="task-list">
-			<t t-foreach="tasks" t-as="task" t-key="task.id">
-				<Task task="task" onDelete.bind="deleteTask"/>
-			</t>
-		</div>
-	</div>
-	`;
-	static components = { Task };  // whenever we define a sub component, it needs to be added to the static components key of its parent
-	
-	nextId = 1;
-	tasks = useState([]);
+    static template = xml /* xml */`
+    <div class="todo-app">
+        <input placeholder="Enter new Task" t-on-keyup="addTask" t-ref="add-input"/>
+        <div class="task-list">
+            <t t-foreach="displayedTasks" t-as="task" t-key="task.id">
+                <Task task="task" />
+            </t>
+        </div>
+        <div class="task-panel" t-if="store.tasks.length">
+            <div class="task-counter">
+                <t t-esc="displayedTasks.length"/>
+                <t t-if="displayedTasks.length lt store.tasks.length"> /
+                    <t t-esc="store.tasks.length"/>
+                </t>
+                task(s)
+            </div>
+            <div>
+                <span t-foreach="['all', 'active', 'completed']" t-as="f" t-key="f"
+                      t-att-class="{active: filter.value === f}"
+                      t-on-click="() => this.setFilter(f)"
+                      t-esc="f"/>
+            </div>
+        </div>
+    </div>
+    `;
+    static components = { Task };  // whenever we define a sub component, it needs to be added to the static components key of its parent
+    
+    // nextId = 1;
+    // tasks = useState([]);
 
-	addTask(e) {
-		if (e.keyCode === 13) {
-			const text = e.target.value.trim();
-			e.target.value = "";
-			if (text) {
-				const newTask = {
-					id: this.nextId++,
-					text: text,
-					isCompleted: false,
-				};
-				this.tasks.push(newTask);
-			}
-		}
-	}
+    setup() {
+        const inputRef = useRef("add-input");
+        onMounted( () => inputRef.el.focus());
+        this.store = useStore();
+        this.filter = useState({ value: "all"});
+    }
 
-	// deleting task
-	deleteTask (task) {
-		const index = this.tasks.findIndex( t => t.id === task.id);
-		this.tasks.splice(index, 1)
-	}
+    addTask(e) {
+        if (e.keyCode === 13) {
+            this.store.addTask(e.target.value);
+            e.target.value = ""
+        }
+    }
 
-	setup() {
-		const inputRef = useRef("add-input");
-		onMounted( () => inputRef.el.focus() );
-	}
+    get displayedTasks() {
+        const tasks = this.store.tasks;
+        switch (this.filter.value) {
+            case "active": return tasks.filter(t => !t.isCompleted);
+            case "completed": return tasks.filter(t => t.isCompleted)
+            case "all": return tasks;
+        }
+    }
+
+    setFilter(filter) {
+        this.filter.value = filter;
+    }
 }
 
-mount(Root, document.body, {dev: true});
+const env = {
+    store: createTaskStore(),
+};
+
+
+mount(Root, document.body, {dev: true, env});
