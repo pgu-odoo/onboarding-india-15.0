701c729f50235d8cf25246773c3795079cde886d
Nishit Munjani <muni@odoo.com>
2022-08-31 11:23:59 +0530

    [ADD] Library Module

---
+++ library_management/__manifest__.py
@@ -0,0 +1,32 @@
+#_*_ coding: utf-8 _*_
+
+{
+	'name' : 'Library Management' , 
+
+	'summary' : """Management app to manage library""" ,
+
+	'description' : """
+		Management Module to manage library:
+		- Courses
+		- sessions
+		- Attendes
+	""",
+
+	'author' : 'Odoo' , 
+
+	'website' : 'https://www.odoo.com',
+
+	'category' : 'Training',
+
+	'version' : '0.1' ,
+
+	'depends' : ['base'],
+
+	'data' : [
+
+	],
+
+	'demo' : [
+
+	],
+}
