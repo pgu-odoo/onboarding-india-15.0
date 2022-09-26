7da72697dfcda88621d4c91cb50e87f8db607d46
depr-odoo <depr@odoo.com>
2022-09-26 18:38:59 +0530

    [ADD] Basic Module

---
+++ library_management /__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Library Management',
+    'version': '1.0',
+    'summary' : """Manage the books"""
+    'description': """
+        Library Management for manage the books
+     """,
+     'author': "odoo",
+
+    'website': 'https://www.odoo.com',
+    'category' : 'Management',
+
+    'depends': ['base'],
+
+    'data': [
+       
+    ],
+
+    'demo': [
+       
+    ],
+    
+}
