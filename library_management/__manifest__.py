7000673f9d73c148e3637f899844965db5f10e9b
depr-odoo <depr@odoo.com>
2022-09-29 14:07:59 +0530

    [ADD] Basic Module

--- library_management/__manifest__.py
+++ library_management/__manifest__.py
@@ -16,7 +16,7 @@
         'security/library_security.xml',
         'security/ir.model.access.csv',
         'views/library_menuitem.xml',
-        'views/book_views.xml',
+        
      ],
 
      'demo': [
